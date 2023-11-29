03aa47385d1b553853e013a88235c36894b6e4a1
Derek Kolb <dkolb1@ivytech.edu>
2023-11-29 02:42:52 -0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
+'''
+Kolb, Derek W. 
+Nov 2023
+SDEV 220 DJango Tutorials
+https://tutorial.djangogirls.org/en/dynamic_data_in_templates/
+'''
+
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  return render(request, 'blog/post_list.html', {'posts': posts})
