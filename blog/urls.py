d5de4bc97c9702a2dc7ceea39ad2417bc523f45c
Derek Kolb <dkolb1@ivytech.edu>
2023-12-10 06:32:56 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 #SDEV 220 M05 Tutorial - Django Deploy and Views
+#SDEV 220 M05 TUtorial - Django Forms, Data, and Security
 #https://tutorial.djangogirls.org/en/django_urls/
 
 from django.urls import path
@@ -6,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
